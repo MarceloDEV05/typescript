55b412fb40b07ea94244b62952fa8bba42ea04be
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2023-12-19 17:19:56 -0300

    typescript

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiply = void 0;
+function multiply(a, b) {
+    return a * b;
+}
+exports.multiply = multiply;
+console.log(multiply(3, 5));
+//npx tsc main.ts para compilar manualmente o typescript
---
+++ main.ts
@@ -0,0 +1,9 @@
+function multiply(a:number, b:number): number{
+    return a * b
+}
+
+console.log(multiply(3, 5))
+
+export {multiply}
+
+//npx tsc main.ts para compilar manualmente o typescript
