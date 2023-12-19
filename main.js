a57188b0e3e46be3d4129f902569a7c7226c76e5
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2023-12-19 18:45:06 -0300

    primeiros codigos em ts

--- main.js
+++ main.js
@@ -7,3 +7,7 @@ function multiply(a, b) {
 exports.multiply = multiply;
 console.log(multiply(3, 5));
 //npx tsc main.ts para compilar manualmente o typescript
+var applyDesc = function (price, percent) {
+    return price - (price * (percent / 100));
+};
+console.log(applyDesc(100, 25));
--- main.ts
+++ main.ts
@@ -7,3 +7,9 @@ console.log(multiply(3, 5))
 export {multiply}
 
 //npx tsc main.ts para compilar manualmente o typescript
+
+const applyDesc = (price:number, percent:number): number =>{
+    return price - (price * (percent / 100))
+}
+
+console.log(applyDesc(100, 25))
